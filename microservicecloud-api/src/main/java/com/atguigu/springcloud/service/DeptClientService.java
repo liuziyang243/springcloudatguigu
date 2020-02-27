2384b75e7ee8171c9c2d23eaff6c15a9a856e831
liuziyang <liuziyang243@gmail.com>
2020-02-27 15:54:11 +0800

    修改错误

---
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author lzy
+ * @create 2020-02-27 上午11:04
+ */
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService {
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    boolean add(Dept dept);
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    Dept get(Integer id);
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    List<Dept> list();
+}
--- microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,34 +20,24 @@ import java.util.List;
 @RestController
 public class DeptController_Consumer {
 
-    //    private static final String REST_URL_PREFIX = "http://localhost:8001";
-    private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+    @Autowired
+    private DeptClientService service;
 
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("/consumer/dept/add")
     public boolean add(Dept dept) {
-        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+        return service.add(dept);
     }
 
     @RequestMapping("/consumer/dept/get/{id}")
     public Dept get(@PathVariable("id") Integer id) {
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+        return service.get(id);
     }
 
     @RequestMapping("/consumer/dept/list")
     public List<Dept> list() {
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
-    }
-
-    /**
-     * 测试消费端可以调用服务发现
-     *
-     * @return
-     */
-    @RequestMapping("/consumer/dept/discovery")
-    public Object discovery() {
-        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
+        return service.list();
     }
 }
